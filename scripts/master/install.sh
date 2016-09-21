f744c3f9e8a80aadd242b6ba4704c4d46efbfb7b
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 16:01:26 +0200

    Update install.sh

--- scripts/master/install.sh
+++ scripts/master/install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-cd $HOME
-
 ### nodejs, npm
+cd $HOME
 tar -C /usr/local --strip-components 1 -xJf node-v4.5.0-linux-x64.tar.xz
 
 ### hyperflow
+cd /
 npm install https://github.com/hopped/hyperflow/archive/develop.tar.gz
+npm install https://github.com/dice-cyfronet/hyperflow-monitoring-plugin/archive/master.tar.gz
 
 ### hyperflow-ui
 mkdir /hyperflow-ui
-tar zxv --no-same-owner --no-same-permissions -f hyperflow-ui.tar.gz -C /hyperflow-ui
+cd $HOME && tar zxv --no-same-owner --no-same-permissions -f hyperflow-ui.tar.gz -C /hyperflow-ui
