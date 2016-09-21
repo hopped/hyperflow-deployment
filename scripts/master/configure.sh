b091e6a1e4be09e3fadab1fb4cf2422aa7e87b5b
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 16:14:19 +0200

    Update configure.sh

--- scripts/master/configure.sh
+++ scripts/master/configure.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo amqp://$PUBLIC_RabbitMQ_Required_by_Master > $HOME/AMQP_URL
 echo redis://$PUBLIC_Redis_Required_by_Master > $HOME/REDIS_URL
+
+cp /hyperflow-deployment/cookbooks/workflows/templates/default/hyperflowMonitoringPlugin.config.js.erb /node_modules/hyperflow-monitoring-plugin/hyperflowMonitoringPlugin.config.js
+
+export AMQP_URL=$(cat $HOME/AMQP_URL)
+export METRIC_COLLECTOR=http://localhost:8086/write?db=hyperflow
+export METRIC_COLLECTOR_TYPE=influxdb
