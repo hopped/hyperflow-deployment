718725fcc803c46ed11008260167eb565e23202c
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 16:25:31 +0200

    Update hyperflowMonitoringPlugin.config.js

--- scripts/master/conf/hyperflowMonitoringPlugin.config.js
+++ scripts/master/conf/hyperflowMonitoringPlugin.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  metricCollector: $METRIC_COLLECTOR,
-  metricCollectorType: $METRIC_COLLECTOR_TYPE,
-  serverName: $SERVER_NAME,
-  amqpURL: $AMQP_URL,
-  rabbitmqUser: $RABBITMQ_USER,
-  rabbitmqPassword: $RABBITMQ_PASSWORD
+  metricCollector: "$METRIC_COLLECTOR",
+  metricCollectorType: "$METRIC_COLLECTOR_TYPE",
+  serverName: "$SERVER_NAME",
+  amqpURL: "$AMQP_URL",
+  rabbitmqUser: "$RABBITMQ_USER",
+  rabbitmqPassword: "$RABBITMQ_PASSWORD"
 };
