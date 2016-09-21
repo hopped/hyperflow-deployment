8e391bf8433f8c20d7dabe3a93e2bbc91541a201
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 16:15:52 +0200

    Create hyperflowMonitoringPlugin.config.js

---
+++ scripts/master/conf/hyperflowMonitoringPlugin.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  metricCollector: $METRIC_COLLECTOR,
+  metricCollectorType: $METRIC_COLLECTOR_TYPE,
+  serverName: $SERVER_NAME,
+  amqpURL: $AMQP_URL,
+  rabbitmqUser: $RABBITMQ_USER,
+  rabbitmqPassword: $RABBITMQ_PASSWORD
+};
