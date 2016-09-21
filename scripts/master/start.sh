f05a2e3451e7790d9cbff6ab0f1ed350a9bacb65
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 16:56:23 +0200

    Update start.sh

--- scripts/master/start.sh
+++ scripts/master/start.sh
@@ -4,6 +4,12 @@ S3_REGION=$2
 S3_BUCKET=$3
 S3_PATH=$4
 
+# influxdb
+sudo service influxdb start
+
+# grafana
+sudo service grafana-server start
+
 # Start HyperFlow as a server
 nohup env PORT=51404 WORKDIR=${WORKDIR} S3_BUCKET=${S3_BUCKET} S3_PATH=${S3_PATH} AMQP_URL=$(cat $HOME/AMQP_URL) REDIS_URL=$(cat $HOME/REDIS_URL) /node_modules/hyperflow/bin/hflow start-server -p hyperflow-monitoring-plugin &
 
