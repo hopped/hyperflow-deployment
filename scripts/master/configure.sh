9d396ed388738c7114c02d17dcf15faa2d995057
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 18:36:40 +0200

    Update configure.sh

--- scripts/master/configure.sh
+++ scripts/master/configure.sh
@@ -7,7 +7,6 @@ influx -execute 'CREATE DATABASE hyperflow'
 
 # grafana
 sed -i '/auth.anonymous/{n;n;s/false/true/}' /etc/grafana/grafana.ini
-cp /etc/grafana/grafana.ini /usr/share/grafana/conf/defaults.ini
 cp conf/hyperflow-home.json /usr/share/grafana/public/dashboards/home.json
 sudo service grafana-server start
 while ! echo exit | nc localhost 3000; do sleep 1; done
