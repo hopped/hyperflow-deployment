eec2103f3e11a363245f9f30fbadceb7d1349bb2
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 18:15:34 +0200

    Update install.sh

--- scripts/master/install.sh
+++ scripts/master/install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+echo amqp://$PUBLIC_RabbitMQ_Required_by_Master > $HOME/AMQP_URL
+echo redis://$PUBLIC_Redis_Required_by_Master > $HOME/REDIS_URL
+
+### monitoring plugin for hyperflow
+export METRIC_COLLECTOR=http://localhost:8086/write?db=hyperflow
+export METRIC_COLLECTOR_TYPE=influxdb
+export AMQP_URL=$(cat $HOME/AMQP_URL)
+export RABBITMQ_USER=guest
+export RABBITMQ_PASSWORD=guest
+export SERVER_NAME=HyperFlow
+cat conf/hyperflowMonitoringPlugin.config.js | envsubst > /node_modules/hyperflow-monitoring-plugin/hyperflowMonitoringPlugin.config.js
+
 ### nodejs, npm
 cd $HOME
 tar -C /usr/local --strip-components 1 -xJf node-v4.5.0-linux-x64.tar.xz
