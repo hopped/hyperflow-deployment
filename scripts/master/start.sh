658000803b54ed9c7ddd98394518a59d0fdfde23
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 15:19:31 +0200

    Update start.sh

--- scripts/master/start.sh
+++ scripts/master/start.sh
@@ -5,7 +5,7 @@ S3_BUCKET=$3
 S3_PATH=$4
 
 # Start HyperFlow as a server
-nohup env PORT=51404 WORKDIR=${WORKDIR} S3_BUCKET=${S3_BUCKET} S3_PATH=${S3_PATH} AMQP_URL=$(cat /hyperflow-deployment/AMQP_URL) REDIS_URL=$(cat /hyperflow-deployment/REDIS_URL) /node_modules/hyperflow/bin/hflow start-server -p hyperflow-monitoring-plugin &
+nohup env PORT=51404 WORKDIR=${WORKDIR} S3_BUCKET=${S3_BUCKET} S3_PATH=${S3_PATH} AMQP_URL=$(cat $HOME/AMQP_URL) REDIS_URL=$(cat $HOME/REDIS_URL) /node_modules/hyperflow/bin/hflow start-server -p hyperflow-monitoring-plugin &
 
 # Run UI on port 80
 export AWS_S3_REGION=${S3_REGION}
