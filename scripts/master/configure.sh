e050f669b1a2d9f1d23cb5bacfec613014f5b896
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-21 18:15:22 +0200

    Update configure.sh

--- scripts/master/configure.sh
+++ scripts/master/configure.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-echo amqp://$PUBLIC_RabbitMQ_Required_by_Master > $HOME/AMQP_URL
-echo redis://$PUBLIC_Redis_Required_by_Master > $HOME/REDIS_URL
-
-### monitoring plugin for hyperflow
-export METRIC_COLLECTOR=http://localhost:8086/write?db=hyperflow
-export METRIC_COLLECTOR_TYPE=influxdb
-export AMQP_URL=$(cat $HOME/AMQP_URL)
-export RABBITMQ_USER=guest
-export RABBITMQ_PASSWORD=guest
-export SERVER_NAME=HyperFlow
-cat conf/hyperflowMonitoringPlugin.config.js | envsubst > /node_modules/hyperflow-monitoring-plugin/hyperflowMonitoringPlugin.config.js
 
 # influxdb
 sudo service influxdb start
