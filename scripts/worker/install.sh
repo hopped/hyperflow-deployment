2c8a40380941cda66eee1980ae06104df62155c3
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 14:01:34 +0200

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -4,11 +4,17 @@ echo "START:install.sh"
 echo "ENV:RabbitMQ " $PUBLIC_RabbitMQ_Required_By_Worker
 echo amqp://$PUBLIC_RabbitMQ_Required_By_Worker > $HOME/AMQP_URL
 
+sudo apt-get install mencoder libgomp1
+
 cd $HOME
 
 ### povray
 tar xvfz povlinux-3.6.tgz
 cd povray-3.6 && ./install -no-arch-check
+
+### md simulation
+cd $HOME/hyperflow-deployment/binaries && tar xvfz md_v4_omp.tgz
+mv $HOME/hyperflow-deployment/binaries/MD_v4_OMP /
 
 ### ruby, mime-types, amqp-executor
 tar -C /usr/local -xzf ruby-2.1.4.tgz
