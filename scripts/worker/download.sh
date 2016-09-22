989acf8448c1b635e67d87bf8e6027251dc2f2b6
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 13:54:50 +0200

    Update download.sh

--- scripts/worker/download.sh
+++ scripts/worker/download.sh
@@ -3,6 +3,9 @@ echo "START:download.sh"
 
 cd $HOME
 
+### povray
+wget http://gd.tuwien.ac.at/graphics/raytracing/povray/Official/Linux/povlinux-3.6.tgz
+
 ### ruby
 wget https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
 
