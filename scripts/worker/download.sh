d3bf341363b3931ff9e78e0d022861f09f5b10b0
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 13:01:47 +0200

    Update download.sh

--- scripts/worker/download.sh
+++ scripts/worker/download.sh
@@ -3,9 +3,6 @@ echo "START:download.sh"
 
 cd $HOME
 
-### nodejs, npm
-wget https://nodejs.org/dist/v4.5.0/node-v4.5.0-linux-x64.tar.xz
-
 ### ruby
 wget https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
 
