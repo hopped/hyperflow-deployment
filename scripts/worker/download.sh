f81c26cd8b9cb887c23ba786fdfeb959d7b589bb
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 12:42:37 +0200

    Update download.sh

--- scripts/worker/download.sh
+++ scripts/worker/download.sh
@@ -6,9 +6,6 @@ cd $HOME
 ### nodejs, npm
 wget https://nodejs.org/dist/v4.5.0/node-v4.5.0-linux-x64.tar.xz
 
-### hyperflow-ui
-wget https://github.com/hopped/hyperflow-ui/releases/download/0.2/hyperflow-ui.tar.gz
-
 ### ruby
 wget https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
 
