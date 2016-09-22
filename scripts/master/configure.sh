fc1d640397cbdf3f087eb9c9fdf8c7fb413fbfe7
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 09:52:02 +0200

    Update configure.sh

--- scripts/master/configure.sh
+++ scripts/master/configure.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "START:configure.sh"
 
 # influxdb
 sudo service influxdb start
@@ -14,3 +15,5 @@ cp conf/hyperflow-home.json /usr/share/grafana/public/dashboards/home.json
 sudo service grafana-server start
 while ! echo exit | nc localhost 3000; do sleep 1; done
 curl -XPOST http://admin:admin@localhost:3000/api/datasources -H "Content-Type: application/json;charset=UTF-8" -d '{ "name": "hyperflow", "type": "influxdb", "access": "proxy", "url": "http://localhost:8086", "password": "root", "user": "root", "database": "hyperflow", "basicAuth": false, "basicAuthUser": "", "basicAuthPassword": "", "withCredentials": false, "isDefault": true }'
+
+echo "END:configure.sh"
