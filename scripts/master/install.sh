1118a6866cca72da8e1f7765f2d7eef59db1bf81
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 09:51:32 +0200

    Update install.sh

--- scripts/master/install.sh
+++ scripts/master/install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
+echo "START: install.sh"
+
+echo "ENV:RabbitMQ" $PUBLIC_RabbitMQ_Required_by_Master
+echo "ENV:Redis" $PUBLIC_Redis_Required_by_Master
 
 echo amqp://$PUBLIC_RabbitMQ_Required_by_Master > $HOME/AMQP_URL
 echo redis://$PUBLIC_Redis_Required_by_Master > $HOME/REDIS_URL
-
-### monitoring plugin for hyperflow
-export METRIC_COLLECTOR=http://localhost:8086/write?db=hyperflow
-export METRIC_COLLECTOR_TYPE=influxdb
-export AMQP_URL=$(cat $HOME/AMQP_URL)
-export RABBITMQ_USER=guest
-export RABBITMQ_PASSWORD=guest
-export SERVER_NAME=HyperFlow
-cat conf/hyperflowMonitoringPlugin.config.js | envsubst > /node_modules/hyperflow-monitoring-plugin/hyperflowMonitoringPlugin.config.js
 
 ### nodejs, npm
 cd $HOME
@@ -25,6 +20,17 @@ npm install https://github.com/dice-cyfronet/hyperflow-monitoring-plugin/archive
 mkdir /hyperflow-ui
 cd $HOME && tar zxv --no-same-owner --no-same-permissions -f hyperflow-ui.tar.gz -C /hyperflow-ui
 
+### monitoring plugin for hyperflow
+export METRIC_COLLECTOR=http://localhost:8086/write?db=hyperflow
+export METRIC_COLLECTOR_TYPE=influxdb
+export AMQP_URL=$(cat $HOME/AMQP_URL)
+export RABBITMQ_USER=guest
+export RABBITMQ_PASSWORD=guest
+export SERVER_NAME=HyperFlow
+cat conf/hyperflowMonitoringPlugin.config.js | envsubst > /node_modules/hyperflow-monitoring-plugin/hyperflowMonitoringPlugin.config.js
+
 ### influxdb, grafana
 sudo apt-get update
 sudo apt-get install -y influxdb grafana
+
+echo "END:install.sh"
