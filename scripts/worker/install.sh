42e08d87cb32c15721cb781c7f191b473f016ac9
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 12:41:49 +0200

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -9,9 +9,10 @@ cd $HOME
 ### nodejs, npm
 tar -C /usr/local --strip-components 1 -xJf node-v4.5.0-linux-x64.tar.xz
 
-### ruby, mime-types
+### ruby, mime-types, amqp-executor
 tar -C /usr/local -xzf ruby-2.1.4.tgz
 gem install mime-types
+gem install --no-ri --no-rdoc hyperflow-amqp-executor
 
 
 echo "END:install.sh"
