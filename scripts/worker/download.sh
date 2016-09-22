a9899b74d34079cce8d4734c9a48622b320a4921
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 11:12:51 +0200

    Create download.sh

---
+++ scripts/worker/download.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "START:download.sh"
+
+cd $HOME
+
+### nodejs, npm
+wget https://nodejs.org/dist/v4.5.0/node-v4.5.0-linux-x64.tar.xz
+
+### hyperflow-ui
+wget https://github.com/hopped/hyperflow-ui/releases/download/0.2/hyperflow-ui.tar.gz
+
+### ruby
+curl https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz -o - | tar xzf - -C /usr/local
+
+echo "END:download.sh"
