f4dbfef4f60365acb55884a8e5c3028fdaf81b27
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 13:56:22 +0200

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -6,6 +6,10 @@ echo amqp://$PUBLIC_RabbitMQ_Required_By_Worker > $HOME/AMQP_URL
 
 cd $HOME
 
+### povray
+tar xvfz povlinux-3.6.tgz
+cd povray-3.6 && ./install -no-arch-check
+
 ### ruby, mime-types, amqp-executor
 tar -C /usr/local -xzf ruby-2.1.4.tgz
 gem install mime-types
