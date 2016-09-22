db653b7ac7590aac86d1eb929907373c55c7e736
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 13:02:02 +0200

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -6,9 +6,6 @@ echo amqp://$PUBLIC_RabbitMQ_Required_By_Worker > $HOME/AMQP_URL
 
 cd $HOME
 
-### nodejs, npm
-tar -C /usr/local --strip-components 1 -xJf node-v4.5.0-linux-x64.tar.xz
-
 ### ruby, mime-types, amqp-executor
 tar -C /usr/local -xzf ruby-2.1.4.tgz
 gem install mime-types
