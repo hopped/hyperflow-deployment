96b61d900416373ab6f1559fbb54bf5c3319ef46
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 15:35:58 +0200

    Update download.sh

--- scripts/worker/download.sh
+++ scripts/worker/download.sh
@@ -6,6 +6,9 @@ cd $HOME
 ### povray
 wget http://gd.tuwien.ac.at/graphics/raytracing/povray/Official/Linux/povlinux-3.6.tgz
 
+### md simulation
+wget https://github.com/hopped/molecular-dynamics-simulation/archive/master.zip
+
 ### ruby
 wget https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
 
