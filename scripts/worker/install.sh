8e3fe23a31843c1c168a3e8dcfd0ea8681e9fa09
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-22 11:18:13 +0200

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-
 echo "START:install.sh"
+
+echo "ENV:RabbitMQ " $PUBLIC_RabbitMQ_Required_By_Hyperflow
+echo "ENV:Redis " $PUBLIC_Redis_Required_By_Hyperflow
+
+echo amqp://$PUBLIC_RabbitMQ_Required_By_Hyperflow > $HOME/AMQP_URL
+echo redis://$PUBLIC_Redis_Required_By_Hyperflow > $HOME/REDIS_URL
 
 cd $HOME
 
