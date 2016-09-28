282b2579198d790caeab8c6898a26255e50906f9
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-28 11:23:29 +0200

    Update download.sh

--- scripts/worker/download.sh
+++ scripts/worker/download.sh
@@ -12,4 +12,7 @@ wget https://github.com/hopped/molecular-dynamics-simulation/archive/master.zip
 ### ruby
 wget https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
 
+### hyperflow-amqp-executor
+wget https://dl.dropboxusercontent.com/u/81819/executor.tar.gz
+
 echo "END:download.sh"
