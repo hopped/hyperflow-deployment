fc52dd5c347b11df06d097604b11a7dadab30b04
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-28 11:27:48 +0200

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -9,7 +9,7 @@ sudo apt-get install -y mencoder libgomp1 build-essential libxml2-dev zlib1g-dev
 
 ### povray
 cd $HOME
-tar xvfz povlinux-3.6.tgz
+tar xfz povlinux-3.6.tgz
 cd povray-3.6 && ./install -no-arch-check
 
 ### md simulation
@@ -19,11 +19,8 @@ cd /MD_v4_MPI/src && make TARGET=RELEASE MPI=1 ARCH=CPU COMPILER=mpi -j4 && cp C
 
 ### ruby, mime-types, amqp-executor
 cd $HOME
-JOBS=$[$(nproc)-1]
 tar -C /usr/local -xzf ruby-2.1.4.tgz
-gem install bundle
-bundle config --global jobs $JOBS
-gem install --no-ri --no-rdoc mime-types
-gem install --no-ri --no-rdoc hyperflow-amqp-executor
+tar -C / -xf executor.tar.gz
+gem install --no-ri --no-rdoc bundler
 
 echo "END:install.sh"
