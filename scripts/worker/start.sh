e16506628c570d62145735f8df892e0912a0a62d
Dennis Hoppe <hopped@users.noreply.github.com>
2016-09-28 11:29:37 +0200

    Update start.sh

--- scripts/worker/start.sh
+++ scripts/worker/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "START:start.sh"
 
-env AMQP_URL=$(cat $HOME/AMQP_URL) hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml
+cd /hyperflow-amqp-executor
+env AMQP_URL=$(cat $HOME/AMQP_URL) bundle exec bin/hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml
