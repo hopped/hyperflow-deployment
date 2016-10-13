be902432d087892caad11743d72f66c6e456d650
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-10-13 11:48:38 +0200

    replaced with simple dummy scripts (sleep 1)

--- scripts/worker/start.sh
+++ scripts/worker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "START:start.sh"
 
-cd /hyperflow-amqp-executor
-env AMQP_URL=$(cat $HOME/AMQP_URL) bundle exec bin/hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml
+while :; do
+  sleep 300
+done
+
+echo "END:start.sh"
