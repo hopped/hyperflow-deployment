7637889741373101d3d3411922ea49b015a30c43
Dennis Hoppe <hopped@users.noreply.github.com>
2016-10-14 13:57:56 +0200

    Update download.sh

--- scripts/worker/download.sh
+++ scripts/worker/download.sh
@@ -4,12 +4,9 @@ echo "START:download.sh"
 cd $HOME
 
 ### povray
-wget http://gd.tuwien.ac.at/graphics/raytracing/povray/Official/Linux/povlinux-3.6.tgz
+wget -q http://gd.tuwien.ac.at/graphics/raytracing/povray/Official/Linux/povlinux-3.6.tgz
 
 ### ruby
-wget https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
-
-### hyperflow-amqp-executor
-wget https://dl.dropboxusercontent.com/u/81819/executor.tar.gz
+wget -q https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
 
 echo "END:download.sh"
