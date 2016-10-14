05be6cb1bc1de95341f5ae45dc67dacda5df616b
Dennis Hoppe <hopped@users.noreply.github.com>
2016-10-14 14:17:34 +0200

    Update configure.sh

--- scripts/worker/configure.sh
+++ scripts/worker/configure.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 echo "START:configure.sh"
 
-sleep 1
+export AMQP_URL=$(cat $HOME/AMQP_URL)
+export THREADS=$(nproc)
+export STORAGE=cloud
+export AWS_REGION=$1
+export AWS_ACCESS_KEY_ID=$2
+export AWS_SECRET_ACCESS_KEY=$3
+
+cat conf/hyperflow-amqp-executor.yml | envsubst > /etc/hyperflow-amqp-executor.yml
 
 echo "END:configure.sh"
