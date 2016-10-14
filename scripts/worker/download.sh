8d87e0ae632e791a04fa14f3c590bc8833a1e782
Dennis Hoppe <hopped@users.noreply.github.com>
2016-10-14 14:16:41 +0200

    Update download.sh

--- scripts/worker/download.sh
+++ scripts/worker/download.sh
@@ -6,6 +6,9 @@ cd $HOME
 ### povray
 wget -q http://gd.tuwien.ac.at/graphics/raytracing/povray/Official/Linux/povlinux-3.6.tgz
 
+### hyperflow-amqp-executor
+wget -q https://dl.dropboxusercontent.com/u/81819/executor.tar.gz
+
 ### ruby
 wget -q https://s3.amazonaws.com/pkgr-buildpack-ruby/current/ubuntu-14.04/ruby-2.1.4.tgz
 
