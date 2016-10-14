b6c205b5e343e76f71c0a60019aef3ab5e2a9486
Dennis Hoppe <hopped@users.noreply.github.com>
2016-10-14 14:17:58 +0200

    Update start.sh

--- scripts/worker/start.sh
+++ scripts/worker/start.sh
@@ -1,8 +1,6 @@
+
 #!/bin/bash
 echo "START:start.sh"
 
-while :; do
-  sleep 300
-done
-
-echo "END:start.sh"
+cd /hyperflow-amqp-executor
+env AMQP_URL=$(cat $HOME/AMQP_URL) bundle exec bin/hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml
