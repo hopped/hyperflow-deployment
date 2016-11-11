d316e1f283b9cf58c7d1d4879449ca078e6e6bca
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-11 11:37:48 +0100

    Update configure.sh

--- scripts/nfs/configure.sh
+++ scripts/nfs/configure.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 echo "START:configure.sh"
 
+# configure nfs server
+mkdir /opt/shared
+chmod -R 777 /opt/shared
+echo "/opt/shared *(rw,sync,no_root_squash,fsid=1,no_subtree_check,insecure)" > /etc/exports
+exportfs -a
+echo "nfs		2049/tcp" >> /etc/services
+echo "nfs		111/udp" >> /etc/services
+
+# source
 . /etc/default/nfs-kernel-server
 . /etc/default/nfs-common
 rpcbind
