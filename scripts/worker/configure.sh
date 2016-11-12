1915ea9c35bdbf36dabe76f481f73d15838a1956
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-11-12 16:04:43 +0100

    added missing configure changes

--- scripts/worker/configure.sh
+++ scripts/worker/configure.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 echo "START:configure.sh"
 
-export AMQP_URL=$(cat $HOME/AMQP_URL)
-export THREADS=$(nproc)
-export STORAGE=cloud
-export AWS_REGION=$1
-export AWS_ACCESS_KEY_ID=$2
-export AWS_SECRET_ACCESS_KEY=$3
-
-cat conf/hyperflow-amqp-executor.yml | envsubst > /etc/hyperflow-amqp-executor.yml
-
 # nfs mount point
 mkdir /opt/shared
 chown -R 777 /opt/shared
