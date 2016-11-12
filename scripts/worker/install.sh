cf059043039a6748f54301acfa281c37fc283106
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-11-12 10:40:37 +0100

    mount /opt/shared

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -2,6 +2,7 @@
 echo "START:install.sh"
 
 echo "ENV:RabbitMQ " $PUBLIC_RabbitMQ_Required_by_Worker
+echo "ENV:NFS " $PUBLIC_NFSServer_Required_by_Worker
 echo amqp://$PUBLIC_RabbitMQ_Required_by_Worker > $HOME/AMQP_URL
 
 ### extra dependencies for amqp-executor mainly
@@ -11,6 +12,11 @@ sudo apt-get install -y mencoder libgomp1 build-essential libxml2-dev zlib1g-dev
 cd $HOME
 tar -xzf povlinux-3.6.tgz
 cd povray-3.6 && ./install -no-arch-check
+
+### nfs client
+mkdir /opt/shared
+chmod -R 777 /opt/shared
+mount -t nfs4 "$PUBLIC_NFSServer_Required_by_Worker":/opt/shared /opt/shared
 
 ### ruby, mime-types, amqp-executor
 cd $HOME
