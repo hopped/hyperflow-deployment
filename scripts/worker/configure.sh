c66a4999a7f2e6af0c162129eda21f36b3199daf
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-13 14:17:59 +0100

    Update configure.sh

--- scripts/worker/configure.sh
+++ scripts/worker/configure.sh
@@ -10,10 +10,4 @@ export AWS_SECRET_ACCESS_KEY=$3
 
 cat conf/hyperflow-amqp-executor.yml | envsubst > /etc/hyperflow-amqp-executor.yml
 
-# nfs mount point
-mkdir /opt/shared
-chown -R 777 /opt/shared
-rpcbind
-mount -t nfs4 -o proto=tcp,port=2049 $PUBLIC_NFSServer_Required_by_Worker:/opt/shared /opt/shared
-
 echo "END:configure.sh"
