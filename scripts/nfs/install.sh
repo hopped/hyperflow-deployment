c6863ed1160fb851d087b53eb9da682b6e0acfd1
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-14 14:09:02 +0100

    Update install.sh

--- scripts/nfs/install.sh
+++ scripts/nfs/install.sh
@@ -4,8 +4,4 @@ echo "START:install.sh"
 # install dependencies
 apt-get install -y nfs-kernel-server nfs-common
 
-### md simulation
-cd ../../binaries
-tar -C /opt/shared/ -xf md_v4_mpi.tar.gz
-
 echo "END:install.sh"
