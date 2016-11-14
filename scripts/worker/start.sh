0ff9544e09f961280397ac33096536f55eed711f
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-14 10:14:36 +0100

    Update start.sh

--- scripts/worker/start.sh
+++ scripts/worker/start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "START:start.sh"
 
-env AMQP_URL=$(cat $HOME/AMQP_URL) hyperflow-amqp-metric-collector &
+# get visor information
+VISOR_HOST=$(ip route show | awk '/default/ {print $3}')
+
+### LOCATE PORT OF TELNET SERVER
+PORTS=( $(curl -s $VISOR_HOST:31415/monitors | jq '.[] | select(.port != null) | .port') )
+VISOR_PORT=${PORTS[0]}
+
+env AMQP_URL=$(cat $HOME/AMQP_URL) VISOR_HOST=$VISOR_HOST VISOR_PORT=$VISOR_PORT hyperflow-amqp-metric-collector &
 env AMQP_URL=$(cat $HOME/AMQP_URL) hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml
