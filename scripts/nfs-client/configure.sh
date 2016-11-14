640f1e7458a7c004115ee9a5d44a82385f217d28
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-14 13:36:31 +0100

    Update configure.sh

--- scripts/nfs-client/configure.sh
+++ scripts/nfs-client/configure.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "START:configure.sh"
 
+# nfs ip (remove port)
+NFS_IP=$PUBLIC_NFSServer_Required_by_Worker
+NFS_IP=${NFS_IP%%:*}
+
 # nfs mount point
 mkdir /opt/shared
 chown -R 777 /opt/shared
 rpcbind
-mount -t nfs4 $PUBLIC_NFSServer_Required_by_Worker:/opt/shared /opt/shared
+mount -t nfs4 $NFS_IP:/opt/shared /opt/shared
 
 echo "END:configure.sh"
