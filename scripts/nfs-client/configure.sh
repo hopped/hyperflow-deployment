a97fddb653af2e671e9dc728d7ec4ca24b5faf49
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-14 09:50:43 +0100

    Create configure.sh

---
+++ scripts/nfs-client/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "START:configure.sh"
+
+# nfs mount point
+mkdir /opt/shared
+chown -R 777 /opt/shared
+rpcbind
+mount -t nfs4 $PUBLIC_NFSServer_Required_by_Worker:/opt/shared /opt/shared
+
+echo "END:configure.sh"
