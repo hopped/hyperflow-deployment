2715e1b457a8ebe975a5b651f72c268e28419426
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-15 16:35:23 +0100

    Update install.sh

--- scripts/worker/install.sh
+++ scripts/worker/install.sh
@@ -8,13 +8,13 @@ echo amqp://$PUBLIC_RabbitMQ_Required_by_Worker > $HOME/AMQP_URL
 ### extra dependencies for amqp-executor mainly
 sudo apt-get install -y mencoder gettext-base mpich bc openssh-server nfs-common
 
-echo "WORKER: PWD=$PWD"
-
 ### povray
+cd $HOME
 tar -xzf povlinux-3.6.tgz
 cd povray-3.6 && ./install -no-arch-check
 
 ### ruby, mime-types, amqp-executor
+cd $HOME
 tar -C /usr/local -xzf ruby-2.1.4.tgz
 tar -C / -xf executor.tar.gz
 gem install --no-ri --no-rdoc bundler
