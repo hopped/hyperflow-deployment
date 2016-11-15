0d3f4db9faaada2bf6c6ba381afcc6a2e0988164
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-15 10:04:11 +0100

    Update configure.sh

--- scripts/worker/configure.sh
+++ scripts/worker/configure.sh
@@ -14,10 +14,12 @@ cat conf/hyperflow-amqp-executor.yml | envsubst > /etc/hyperflow-amqp-executor.y
 NFS_IP=$PUBLIC_NFSServer_Required_by_Worker
 NFS_IP=${NFS_IP%%:*}
 
+echo "NFS host: " $NFS_IP
+
 # nfs mount point
 mkdir /opt/shared
 chown -R 777 /opt/shared
 rpcbind
-mount -t nfs4 $NFS_IP:/opt/shared /opt/shared
+mount -t nfs4 "$NFS_IP":/opt/shared /opt/shared
 
 echo "END:configure.sh"
