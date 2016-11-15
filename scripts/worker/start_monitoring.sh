61c514335ad776b4e53e8f859edd88e16ebc5abd
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-15 10:05:59 +0100

    Create start_monitoring.sh

---
+++ scripts/worker/start_monitoring.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# get visor information
+VISOR_HOST=$(ip route show | awk '/default/ {print $3}')
+
+### LOCATE PORT OF TELNET SERVER
+#PORTS=( $(curl -s $VISOR_HOST:31415/monitors | jq '.[] | select(.port != null) | .port') )
+#VISOR_PORT=${PORTS[0]}
+VISOR_PORT=49152
+
+echo "Waiting for visor telnet to launch on $VISOR_HOST:$VISOR_PORT"
+while ! nc -zv $VISOR_HOST $VISOR_PORT; do   
+  sleep 1
+done
+
+echo "Starting metric collector..."
+env AMQP_URL=$(cat $HOME/AMQP_URL) VISOR_HOST=$VISOR_HOST VISOR_PORT=$VISOR_PORT hyperflow-amqp-metric-collector &
