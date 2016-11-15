5596d6c751db13b447d5058e2e5315001959e8e9
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-15 10:01:08 +0100

    Update install.sh

--- scripts/rabbitmq/install.sh
+++ scripts/rabbitmq/install.sh
@@ -2,8 +2,7 @@
 echo "START:install.sh"
 
 ### rabbitmq
-apt-get update
-apt-get install -y rabbitmq-server
+apt-get update && apt-get install -y rabbitmq-server
 
 rabbitmq-plugins enable rabbitmq_management
 
