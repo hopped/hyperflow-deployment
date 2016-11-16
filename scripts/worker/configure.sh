3c02c50053413b6fd6a81a4228f8f26d8978e01c
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-16 18:48:10 +0100

    Update configure.sh

--- scripts/worker/configure.sh
+++ scripts/worker/configure.sh
@@ -3,6 +3,7 @@ echo "START:configure.sh"
 
 echo "NFS Server: $PUBLIC_NFSServer_Required_by_Worker"
 echo "RabbitMQ Mgt Port: $PUBLIC_RabbitMQMgtPort_Required_by_Worker"
+echo "Master Server: $PUBLIC_Master_Required_by_Worker"
 
 export AMQP_URL=$(cat $HOME/AMQP_URL)
 export THREADS=$(nproc)
@@ -21,7 +22,11 @@ mv conf/hyperflow-amqp-metric-collector /usr/local/lib/ruby/gems/2.1.0/gems/hype
 NFS_IP=$PUBLIC_NFSServer_Required_by_Worker
 NFS_IP=${NFS_IP%%:*}
 
+MASTER_IP=$PUBLIC_Master_Required_by_Worker
+MASTER_IP=${MASTER_IP%%:*}
+
 echo "NFS host: " $NFS_IP
+echo "Master host: " $MASTER_IP
 
 # nfs mount point
 mkdir /opt/shared
@@ -31,6 +36,7 @@ mount -t nfs4 "$NFS_IP":/opt/shared /opt/shared
 
 # add update check into run-cmd.sh
 sed -i '56i \    $(curl -sS -u guest:guest "http://'$PUBLIC_RabbitMQMgtPort_Required_by_Worker'/api/consumers" | jq --raw-output .[].channel_details.peer_host > /opt/shared/MD_v4_MPI/hostfile.txt)' /opt/shared/MD_v4_MPI/run-cmd.sh
+sed -i '57i \    $(sed -i "/'$MASTER_IP'/d" /opt/shared/MD_v4_MPI/hostfile.txt' /opt/shared/MD_v4_MPI/run-cmd.sh
 
 # ssh configuration
 echo "    StrictHostKeyChecking no" | sudo tee -a /etc/ssh/ssh_config  
