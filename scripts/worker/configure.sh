3b205972c7d0f75ef905625a567ec297f7382255
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-18 15:13:30 +0100

    Update configure.sh

--- scripts/worker/configure.sh
+++ scripts/worker/configure.sh
@@ -35,8 +35,8 @@ rpcbind
 mount -t nfs4 "$NFS_IP":/MD_v4_MPI /MD_v4_MPI
 
 # add update check into run-cmd.sh
-sed -i '70i \    $(curl -sS -u guest:guest "http://'$PUBLIC_RabbitMQMgtPort_Required_by_Worker'/api/consumers" | jq --raw-output .[].channel_details.peer_host | sort | uniq > /MD_v4_MPI/hostfile.txt)' /MD_v4_MPI/run-cmd.sh
-sed -i '71i \    $(sed -i "/'$MASTER_IP'/d" /MD_v4_MPI/hostfile.txt)' /MD_v4_MPI/run-cmd.sh
+sed -i '25i \    $(curl -sS -u guest:guest "http://'$PUBLIC_RabbitMQMgtPort_Required_by_Worker'/api/consumers" | jq --raw-output .[].channel_details.peer_host | sort | uniq > /MD_v4_MPI/hostfile.txt)' /MD_v4_MPI/run-cmd.sh
+sed -i '26i \    $(sed -i "/'$MASTER_IP'/d" /MD_v4_MPI/hostfile.txt)' /MD_v4_MPI/run-cmd.sh
 
 # ssh configuration
 echo "    StrictHostKeyChecking no" | sudo tee -a /etc/ssh/ssh_config  
