7335c0d11fd26bbde1c5fbc5d5ad15eb2534816f
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-18 11:48:49 +0100

    Update start.sh

--- scripts/worker/start.sh
+++ scripts/worker/start.sh
@@ -5,4 +5,5 @@ echo "START:start.sh"
 bash start-monitoring.sh > start-monitoring.log 2>&1 &
 
 # start the job executor
+cd /
 env AMQP_URL=$(cat $HOME/AMQP_URL) hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml > amqp.log 2>&1 &
