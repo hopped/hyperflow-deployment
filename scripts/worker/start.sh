ee5212a35b67b8c99b371377bd67b27f1a1e2b7f
Dennis Hoppe <hopped@users.noreply.github.com>
2016-11-18 11:53:22 +0100

    Update start.sh

--- scripts/worker/start.sh
+++ scripts/worker/start.sh
@@ -2,8 +2,8 @@
 echo "START:start.sh"
 
 # start monitoring of hyperflow jobs in the background
-bash start-monitoring.sh > start-monitoring.log 2>&1 &
+bash start-monitoring.sh > $HOME/start-monitoring.log 2>&1 &
 
 # start the job executor
 cd /
-env AMQP_URL=$(cat $HOME/AMQP_URL) hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml > amqp.log 2>&1 &
+env AMQP_URL=$(cat $HOME/AMQP_URL) hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml > $HOME/amqp.log 2>&1 &
