78188506de9296811c9bb32609cfe472f67c1ffe
Dennis Hoppe <dennis.hoppe@hlrs.de>
2016-11-18 14:22:46 +0100

    removed cd /

--- scripts/worker/start.sh
+++ scripts/worker/start.sh
@@ -5,5 +5,4 @@ echo "START:start.sh"
 bash start-monitoring.sh > $HOME/start-monitoring.log 2>&1 &
 
 # start the job executor
-cd /
 env AMQP_URL=$(cat $HOME/AMQP_URL) hyperflow-amqp-executor /etc/hyperflow-amqp-executor.yml > $HOME/amqp.log 2>&1 &
